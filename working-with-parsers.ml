4ee1d1865e59b5ccc7270b0ebdcd3d0a11046bd6
david-prv <66866223+david-prv@users.noreply.github.com>
2021-12-14 11:59:58 +0100

    Update working-with-parsers.ml

--- working-with-parsers.ml
+++ working-with-parsers.ml
@@ -1,11 +1,13 @@
 type tree = A | B of tree * tree | C of tree * tree ;;
 type token = AT | BT | CT | LP | RP ;;
 
+(* Folding from the Right *)
 let rec fold f l b =
   match l with
   | [] -> b
   | x :: t -> f x (fold f t b) ;;
 
+(* Palindrom Checker *)
 let is_palindrom s =
   let len = String.length s in
   let rec explode s i a =
@@ -32,19 +34,32 @@ let init_t f n =
     else
       init' f (n-1) ( [f(n-1)] @ a)
   in init' f n [] ;;
-    
+   
+(* Explode  *) 
 let explode s =
   let rec explode' s i a =
     if i = String.length s then a
     else explode' s (i+1) (a @ [String.get s i])
   in explode' s 0 [] ;;
 
+(* Implode *)
 let implode l = fold (fun c b -> String.make 1 c ^ b) l "" ;;
+implode ['\240'; '\159'; '\152'; '\138'] 
 
 let verify c l = match l with
   | [] -> failwith "verify: no token"
   | c'::l -> if c'=c then l else failwith "verify: wrong token" ;;
-          
+       
+(*  Prefix: Redundant parentheses throw exceptions *) 
+let rec tree_pre' l = match l with 
+  | AT::l -> (A,l)
+  | BT::l ->
+      let (t1,l) = tree_pre' l in
+      let (t2,l) = tree_pre' l in
+      (B(t1,t2), l) 
+  | _ -> failwith "tree" ;;
+  
+(* Prefix: Allows redundant parentheses *)
 let rec tree_pre l = match l with
   | LP::l -> let (t,l) = tree_pre l in (t, verify RP l) 
   | AT::l -> (A,l)
@@ -54,16 +69,56 @@ let rec tree_pre l = match l with
       (B(t1,t2), l) 
   | _ -> failwith "tree" ;;
            
+(* Infix: Fully Parenthesized *)
 let rec tree_inf l = 
   let (t,l)= ptree l in tree' t l
 and tree' t l  = match l with
-  | BT::k -> let (t1,k)= ptree k in tree' (B(t,t1)) k
+  | BT :: k -> let (t1,k) = ptree k in tree' (B(t,t1)) k
   | _ -> (t,l) 
 and ptree l = match l with
-  | AT::k -> (A,k)
-  | LP::k -> let (t,k)= tree_inf k in (t, verify RP k)
+  | AT :: k -> (A,k)
+  | LP :: k -> let (t,k) = tree_inf k in (t, verify RP k)
   | _ -> failwith "tree" ;;
-           
+        
+(*  Prefix ABC: Without redundant parentheses *)   
+let rec treeABC_pre' l = match l with
+  | AT :: k-> (A,k)
+  | BT :: k-> let (t1,k) = treeABC_pre' k in
+      let (t2,k)= treeABC_pre' k in
+      (B(t1,t2),k)
+  | CT :: k-> let (t1,k) = treeABC_pre' k in
+      let (t2,k)= treeABC_pre' k in
+      (C(t1,t2),k)
+  | _ -> failwith "tree"   
+     
+(*  Prefix ABC: Allows redundant parentheses *)    
+let rec treeABC_pre l = match l with
+  | AT :: k -> (A,k)
+  | BT :: k -> let (t1,k) = treeABC_pre k in
+      let (t2,k)= treeABC_pre k in
+      (B(t1,t2),k)
+  | CT :: k -> let (t1,k) = treeABC_pre k in
+      let (t2,k)= treeABC_pre k in
+      (C(t1,t2),k)
+  | LP :: k -> let (t,k) = treeABC_pre k in (t, verify RP k)
+  | _ -> failwith "tree"   
+         
+(* Infix ABC: Fully Parenthesized  *)
+let rec treeABC_inf l = 
+  let (t,l) = btree l in tree' t l
+and tree' t l= match l with
+  | CT :: k -> let (t1,k) = btree k in tree' (C(t,t1)) k
+  | _ -> (t,l)
+and btree l = let (t,l) = ptree l in btree' t l
+and btree' t l= match l with
+  | BT :: k -> let (t1,k) = ptree k in btree' (B(t,t1)) k
+  | _ -> (t,l)
+and ptree l= match l with
+  | AT :: k -> (A,k)
+  | LP :: k -> let (t,k) = treeABC_inf k in (t, verify RP k)
+  | _ -> failwith "tree"
+
+(*  Infix Linearization of AB-Trees (for testing) *)
 let rec inf t =
   let ptree t = match t with
     | A -> "A"
@@ -71,11 +126,9 @@ let rec inf t =
   in match t with
   | A -> "A"
   | B(t1,t2) -> inf t1 ^ "B" ^ ptree t2 
-  | _ -> failwith "what" ;;
-           
-implode ['\240'; '\159'; '\152'; '\138'] 
+  | _ -> failwith "tree" ;; 
 
-(* Lexer: Throws Exception for illegal chars *)
+(* Lexer: Throws exception for illegal chars *)
 let lex s =
   let n = String.length s in
   let rec lex i l =
@@ -104,7 +157,7 @@ let lex' s =
       | ' ' | '\n' | '\t' | _ -> lex' (i+1) l 
   in lex' 0 [] ;; 
   
-(* Lexer: Skips comments too *)
+(* Lexer: Ignores illegal chars and skips comments *)
 let lex_ s = 
   let l = explode s in
   let rec skip l =
@@ -139,8 +192,8 @@ let lex_ s =
             | _ -> lex_rec t (LP::akku)
           end 
       | ')' -> lex_rec t (RP::akku)
-      | ' ' | '\n' | '\t' | _ -> lex_rec t akku   
-  in List.rev (lex_rec l [])
+      | ' ' | '\n' | '\t' | _ -> lex_rec t akku 
+  in List.rev (lex_rec l []) 
 
 (* Count: Recursive *)  
 let count c s = 
@@ -153,7 +206,7 @@ let count c s =
   in count' 0 ;;    
 
 (* Count: Tail Recursive *)
-let count_t c s = 
+let count' c s = 
   let n= String.length s in 
   let rec count' i c a= 
     if i>= n then a
@@ -162,5 +215,13 @@ let count_t c s =
           else count' (i+1) c a
   in
   count' 0 c 0 ;; 
-                  
-                  
+       
+(*  *)           
+let rec depost l1 l2 = match l1, l2 with
+  | [], l2 -> l2
+  | AT::l1, l2 -> depost l1 (A::l2)
+  | BT::l1, t2::t1::l2 -> depost l1 (B(t1,t2)::l2)
+  | CT::l1, t2::t1::l2 -> depost l1 (C(t1,t2)::l2)
+  | _, _ -> []
+
+let test = depost (lex "AABABAABBAC") [] 
