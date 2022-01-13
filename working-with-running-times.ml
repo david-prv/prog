87df34e5927baa058f50baacad8b5f78af8b5463
david-prv <66866223+david-prv@users.noreply.github.com>
2022-01-13 09:28:11 +0100

    Update working-with-running-times.ml

--- working-with-running-times.ml
+++ working-with-running-times.ml
@@ -1,3 +1,4 @@
+(* Merge Sort declarations *)
 let rec append l1 l2 = match l1 with
   | [] -> l2
   | x :: l1 -> x :: append l1 l2 
@@ -22,28 +23,3 @@ let rec f n =
   match n with
   | 0 -> []
   | x -> x :: f (n-1)
-
-(* Merge Sort declarations *)
-let rec append l1 l2 = match l1 with
-  | [] -> l2
-  | x :: l1 -> x :: append l1 l2 
-let rec rev l = match l with
-  | [] -> []
-  | x :: l -> append (rev l) [x]
-let rec rev_append l1 l2 = match l1 with
-  | [] -> l2
-  | x :: l1 -> rev_append l1 (x::l2)
-let rec prefixes l = match l with
-  | [] -> [[]]
-  | x :: l -> [] :: List.map (List.cons x) (prefixes l)
-  
-let rec rev' l =
-  match l with
-  | [] -> []
-  | x::l -> rev_append l [x]
-              
-let rec f n =
-  match n with
-  | 0 -> []
-  | x -> x :: f (n-1)
-              
