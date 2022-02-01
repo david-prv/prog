6775cf35a4ab458175067d04fdb051c47938661b
David <66866223+david-prv@users.noreply.github.com>
2022-02-01 15:00:36 +0100

    Update working-with-data-structures.ml

--- working-with-data-structures.ml
+++ working-with-data-structures.ml
@@ -4,3 +4,15 @@ module type CELL = sig
   val get : 'a cell -> 'a
   val set : 'a cell -> 'a -> unit
 end
+
+module Cell : CELL = struct
+  type 'a cell = 'a array
+  let make x = Array.make 1 x
+  let get c = c.(0)
+  let set c x = c.(0) <- x
+end
+
+let enum =
+  let c = Cell.make 0
+  in fun () -> let x = Cell.get c in
+    Cell.set c (x+1); x
