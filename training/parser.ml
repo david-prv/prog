42d3643d327c0055b1675ad5d4a8c04a808aa6df
David <66866223+david-prv@users.noreply.github.com>
2022-02-21 14:11:48 +0100

    Update parser.ml

--- training/parser.ml
+++ training/parser.ml
@@ -1,9 +1,12 @@
-type tree = A | B of tree * tree ;;
-type token = AT | BT | CT | LP | RP ;; 
+
+(* AB-Trees *)
+type tree = A | B of tree * tree ;; 
 
 let explode s = List.init (String.length s) (String.get s) ;;
 let implode l = List.fold_right (fun c s -> String.make 1 c ^ s) l "" ;;
     
+(* Lex any AB/ABC-Tree Linearization to Tokenlist *)
+type token = AT | BT | CT | LP | RP ;; 
 let lex s =
   let n = String.length s in
   let rec lex i l =
@@ -19,20 +22,70 @@ let lex s =
   in lex 0 []
 ;;
 
+(* Prefix - Linearization AB-Trees *)
 let rec prefix t = 
   match t with
   | A -> "A"
   | B(t1, t2) -> "B" ^ prefix t1 ^ prefix t2
 ;;
 
-let rec tree l = match l with
+(* Parse prefix-linearized AB-Trees *)
+let rec prefixl_tree l = match l with
   | AT::l -> (A,l)
   | BT::l ->
-      let (t1,l) = tree l in
-      let (t2,l) = tree l in
+      let (t1,l) = prefixl_tree l in
+      let (t2,l) = prefixl_tree l in
       (B(t1,t2), l)
   | _ -> failwith "tree"
 ;;
 
-let t1 = prefix (B(B(A,A),B(A,A))) ;;
-let tl1 = lex t1 ;;
+(* Infix - Linearization: Fully Parenthesized *)
+let rec infix t =
+  match t with
+  | A -> "A"
+  | B(t1,t2) -> "(" ^ infix t1 ^ "B" ^ infix t2 ^ ")" 
+;; 
+
+let t1 = (B(B(A,A),B(A,A))) ;;
+let pt1 = prefix t1 ;;
+let pt1_lex = lex pt1 ;;
+
+(* ABC-Trees *)
+type ctree = A | B of ctree * ctree | C of ctree * ctree ;;
+
+let ct = (C(B(A,A), C(B(A,A),B(A,A)))) ;;
+
+(* Infix - Linearization: B before C, B and C are left-ass *)
+let infix' t =
+  let rec ctree t =
+    match t with
+    | C(t1,t2) -> ctree t1 ^ "C" ^ btree t2
+    | _ -> btree t
+  and btree t =
+    match t with
+    | B(t1,t2) -> btree t1 ^ "B" ^ atree t2
+    | _ -> atree t
+  and atree t =
+    match t with
+    | A -> "A"
+    | _ -> "(" ^ ctree t ^ ")"
+  in ctree t
+;;
+
+let juxta_infix' t =
+  let rec ctree t =
+    match t with
+    | C(t1,t2) -> ctree t1 ^ "C" ^ btree t2
+    | _ -> btree t
+  and btree t =
+    match t with
+    | B(t1,t2) -> btree t1 ^ atree t2
+    | _ -> atree t
+  and atree t =
+    match t with
+    | A -> "A"
+    | _ -> "(" ^ ctree t ^ ")"
+  in ctree t
+;;
+
+infix' ct ;;
