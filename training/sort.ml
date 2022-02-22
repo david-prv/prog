fe2a96b3ca07fc182c7379a50951f6cc7034e42e
David <66866223+david-prv@users.noreply.github.com>
2022-02-22 22:54:12 +0100

    Update sort.ml

--- training/sort.ml
+++ training/sort.ml
@@ -1,24 +1,34 @@
-let rec split l l1 l2 = match l with
-  | [] -> (l1,l2)
-  | [x] -> (x::l1,l2)
-  | x::y::l -> split l (x::l1) (y::l2)
-let rec merge l1 l2 = match l1, l2 with
-  | [], l2 -> l2
-  | l1, [] -> l1
-  | x::l1, y::l2 when x <= y -> x :: merge l1 (y::l2)
-  | x::l1, y::l2 -> y :: merge (x::l1) l2
-let rec msort l = match l with
-  | x::y::l -> let (l1,l2) = split l [x] [y] in merge (msort l1) (msort l2) 
-  | l -> l
-
-let swap a i j = let buff = a.(i) in (a.(i) <- a.(j);a.(j) <- buff)
-                                     
-let partition a l r =
+let rec partition a l r =
   let x = a.(r) in
   let rec loop i j =
-    if j < i
-    then (swap a i r; i)
+    if j < i then (swap a i r;i)
     else if a.(i) < x then loop (i+1) j
     else if a.(j) >= x then loop i (j-1)
     else (swap a i j; loop (i+1) (j-1))
   in loop l (r-1)
+  
+let qsort a =
+  let rec qsort' l r =
+    if r < l then ()
+    else let m = partition a l r in
+      (qsort' l (m-1); qsort' (m+1) r)
+  in qsort' 0 (Array.length a - 1)
+    
+let rec split l l1 l2 =
+  match l with 
+  | [] -> (l1,l2)
+  | x :: y :: l -> split l (x::l1) (y::l2)
+  | x :: [] -> split l (x::l1) l2
+                 
+let rec merge l1 l2 =
+  match l1,l2 with
+  | [], [] -> []
+  | x :: l, [] -> (x::l)
+  | [], x :: l -> (x::l)
+  | x::l1, y::l2 -> if x < y then x :: merge l1 (y::l2) else y :: merge (x::l1) l2
+                                                               
+let rec msort l =
+  match l with 
+  | x::y::l -> let (l1,l2) = split l [x] [] in merge (msort l1) (msort l2)
+  | x::[] -> [x]
+  | [] -> []
