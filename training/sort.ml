c34cd64ac2fa6c886625e0e3ceabc42037d35bc9
David <66866223+david-prv@users.noreply.github.com>
2022-02-22 17:12:31 +0100

    Update sort.ml

--- training/sort.ml
+++ training/sort.ml
@@ -10,3 +10,15 @@ let rec merge l1 l2 = match l1, l2 with
 let rec msort l = match l with
   | x::y::l -> let (l1,l2) = split l [x] [y] in merge (msort l1) (msort l2) 
   | l -> l
+
+let swap a i j = let buff = a.(i) in (a.(i) <- a.(j);a.(j) <- buff)
+                                     
+let partition a l r =
+  let x = a.(r) in
+  let rec loop i j =
+    if j < i
+    then (swap a i r; i)
+    else if a.(i) < x then loop (i+1) j
+    else if a.(j) >= x then loop i (j-1)
+    else (swap a i j; loop (i+1) (j-1))
+  in loop l (r-1)
