64bbaa1c0a834965ec68a52343ff8fe247a8b882
David <66866223+david-prv@users.noreply.github.com>
2022-05-24 15:23:07 +0200

    Update convert_ppm.sh

--- prog2/scripts/convert_ppm.sh
+++ prog2/scripts/convert_ppm.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-# Gist: https://gist.github.com/david-prv/236c7e44b23f5fa71e4c2819276c9b0e
-
 # check for arguments
 if [ $# -eq 0 ]
 then
@@ -13,19 +11,13 @@ fi
 # check for imagemagick dependency
 pacman -Qi imagemagick > /dev/null
 
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
-	# imagemagick exists
-	convert $1 -compress none -set comment \"\" $2
-	sed -i '/^#/d' $2
-	exit 0
-else
 	# missing dependency
 	echo "Missing dependency 'imagemagick'. Installing..."
 	sudo pacman -S imagemagick --noconfirm
-	
-	# continue converting
-	convert $1 -compress none -set comment \"\" $2
-	sed -i '/^#/d' $2
-	exit 0
 fi
+	
+# converting
+convert $1 -compress none -set comment \"\" $2
+sed -i '/^#/d' $2
